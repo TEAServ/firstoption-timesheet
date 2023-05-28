35f43a12c3bd4cb62f1042687250263a9f86eb9c
TEAServ <92520122+TEAServ@users.noreply.github.com>
2023-05-28 10:58:45 +0300

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -1,3 +1,4 @@
-export const centerData = {BackEndURL:"https://timesheet-backend-15-08-2020.herokuapp.com"}
+export const centerData = {BackEndURL:"https://firstoption-timesheet-backendd.onrender.com"}
+//https://timesheet-backend-15-08-2020.herokuapp.com
 //"https://timesheetbackend-firstoption.herokuapp.com"
 //http://localhost:443
